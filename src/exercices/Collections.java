e2d13eb1ed5a7514dc45fccf5afed2681b798bbe
formation <formation@FORMATI-SUFPLOU.formation.at>
2017-04-21 14:54:28 +0200

    Collections: exercices récap. sur ArrayList

--- src/exercices/Collections.java
+++ src/exercices/Collections.java
@@ -12,19 +12,52 @@ import java.util.Date;
  *
  * @author formation
  */
-public class Collections {
+public class Collections { 
     
     public static void main(String[] args) {
         
+        
+        // Exercices avec ArrayList
+        
+        // 1. Ajouter a,b,c à listeA
+        ArrayList<String> listeA = new ArrayList<>();
+        
+        listeA.add("a");
+        listeA.add("b");
+        listeA.add("c");
+        
+        // 2. Ajouter a,b,c à listeB
+        ArrayList<String> listeB = new ArrayList<>();
+        
+        listeB.add("d");
+        listeB.add("e");
+        listeB.add("f");
+        
+        // 3. Crée listeC = listeA + listeB
+        ArrayList<String> listeC = new ArrayList<>();
+        listeC.addAll(listeA);
+        listeC.addAll(listeB);
+        
+        // 4. Supprime "a" de listeC
+        listeC.remove("a");
+        
+        // 5. Vérifie si 'a' existe ds listeC
+        System.out.println( "C contient 'a' : " + listeC.contains("a") );
+        
+        // 6. Affichie listeC
+        System.out.println( listeC );
+        
         // Liste typée
-        ArrayList<String> titres = new ArrayList<String>();
+//        ArrayList<String> titres = new ArrayList<String>();
         
-        titres.add("le chat et la souris");
-        titres.add("renée la taupe");
-        titres.add("kaplan");
         
-        for( String s : titres )
-            System.out.println(s);
+        
+//        titres.add("le chat et la souris");
+//        titres.add("renée la taupe");
+//        titres.add("kaplan");
+//        
+//        for( String s : titres )
+//            System.out.println(s);
         
         // Liste non typée
 //        ArrayList liste = new ArrayList();
